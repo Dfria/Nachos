93143be0b6824b700ec2c72a0f749fca129ec717
Dfria <jdfria@gmail.com>
2021-12-03 20:41:25 -0800

    edwin

--- Nachos/src/nachos/userprog/UserProcess.java
+++ Nachos/src/nachos/userprog/UserProcess.java
@@ -317,7 +317,7 @@ public class UserProcess {
     /**
      * Release any resources allocated by <tt>loadSections()</tt>.
      */
-    protected void unloadSections() {
+    protected void unloadSections() {	
     }    
 
     /**
